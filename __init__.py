3c13cfd22fd4dad609e0c2984d37a5d9d3427a09
filip <filiplama@gmail.com>
2019-02-28 18:16:53 +0100

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,15 @@
+'''
+ * Created by filip on 28/02/2019
+'''
+
+def __init__():
+    print("sup")
+
+    def my_method():
+        print("wassup")
+        print("ble")
+
+    my_method()
+
+
+__init__()
